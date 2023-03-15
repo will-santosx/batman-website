78ac208e383ee652672e680af70cbc88cdc33546
Willians Santos <taldowill29@gmail.com>
2023-03-15 00:54:51 -0400

    [add] nav-bar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lexend', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
